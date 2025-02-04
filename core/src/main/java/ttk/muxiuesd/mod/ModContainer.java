86a5c08758ec99cf23651084aa3147f5fe6a1fcb
Muxiuesd <m114514012138@outlook.com>
2025-02-04 14:28:21 +0800

    添加Mod容器，将mod存储与加载分离

---
+++ core/src/main/java/ttk/muxiuesd/mod/ModContainer.java
@@ -0,0 +1,64 @@
+package ttk.muxiuesd.mod;
+
+import ttk.muxiuesd.util.Log;
+
+import java.util.HashMap;
+
+/**
+ * 所有mod的储存
+ * 应该是唯一的
+ * */
+public class ModContainer {
+    public final String TAG = this.getClass().getName();
+
+    private static ModContainer singleton;
+    private HashMap<String, Mod> container = new HashMap<>();
+
+    private ModContainer() {
+    }
+
+    public static ModContainer getInstance() {
+        if (singleton == null) {
+            singleton = new ModContainer();
+        }
+        return singleton;
+    }
+
+    public void add(String namespace, Mod mod) {
+        if (this.hasMod(namespace)) {
+            Log.error(TAG, "Mod的命名空间：" + namespace + "已经存在，不可重复添加！！！");
+            return;
+        }
+        this.container.put(namespace, mod);
+    }
+
+    public Mod remove(String namespace) {
+        if (this.hasMod(namespace)) {
+            return this.container.remove(namespace);
+        }
+        Log.error(TAG, "Mod的命名空间：" + namespace + "不存在，无法删除！！！");
+        return null;
+    }
+
+    public Mod get(String namespace) {
+        if (this.hasMod(namespace)) {
+            return container.get(namespace);
+        }
+        return null;
+    }
+
+    public boolean hasMod(String namespace) {
+        return this.container.containsKey(namespace);
+    }
+
+    /**
+     * 获取所有的mod
+     * */
+    protected HashMap<String, Mod> getAllMods () {
+        return this.container;
+    }
+
+    public boolean isEmpty () {
+        return this.container.isEmpty();
+    }
+}
--- core/src/main/java/ttk/muxiuesd/mod/ModLoader.java
+++ core/src/main/java/ttk/muxiuesd/mod/ModLoader.java
@@ -24,21 +24,20 @@ public class ModLoader {
 
     //这个必须是单例模式
     private static ModLoader Instance;
-
+    //mod根目录
     private final String root = "mods/";
     private String lib;
-    private HashMap<String, Mod> mods = new HashMap<>();
+    //private HashMap<String, Mod> mods = new HashMap<>();
 
     private ModLoader() {
-        Log.print(TAG, this.getRootFile().toString());
-        if (!this.getRootFile().exists()) {
-            //新建mods文件夹
-            this.getRootFile().mkdirs();
-            Log.print(TAG, "创建mods文件夹");
-        }else {
-            Log.print(TAG, "mods文件夹已经存在：" + this.getRootFile().path() + "，跳过创建");
-        }
-        //加载底层库
+        this.checkRoot();
+        this.loadLib();
+    }
+
+    /**
+     * 加载mod的底层库
+    * */
+    private void loadLib () {
         FileHandle libFile = Gdx.files.internal("modlib/lib.js");
         this.lib = libFile.readString();
         Log.print(TAG, "模组底层库加载完毕。");
@@ -56,7 +55,7 @@ public class ModLoader {
             this.loadMod(modDirs[i]);
         }
         this.addModEventCaller();
-        Log.print(TAG, "所有模组加载完成！共加载" + mods.size() + "个模组");
+        Log.print(TAG, "所有模组加载完成！共加载" + ModContainer.getInstance().getAllMods().size() + "个模组");
     }
 
     /**
@@ -72,35 +71,42 @@ public class ModLoader {
 
         JsonReader reader = new JsonReader();
         JsonValue info = reader.parse(infoString);
+        String namespace = info.getString("namespace");
 
         if (! Objects.equals(info.getString("name"), "")
-            && ! Objects.equals(info.getString("namespace"), "")
-            && !this.hasMod(info)
+            && ! Objects.equals(namespace, "")
+            && ! this.hasMod(info)
             && ! Objects.equals(info.getString("version"), "")
             && ! Objects.equals(info.getString("author"), "")
             && ! Objects.equals(info.getString("description"), "")
             && ! Objects.equals(info.getString("main"), "")) {
 
+            /*if (ModContainer.getInstance().hasMod(namespace)) {
+                Log.print(TAG, "Mod的命名空间：" + namespace + "已经存在，不可重复添加！！！");
+                return;
+            }*/
             Mod mod = new Mod(info, modDir);
-            this.mods.put(info.get("namespace").asString(), mod);
+            ModContainer.getInstance().add(namespace, mod);
+            //this.mods.put(info.get("namespace").asString(), mod);
 
             Log.print(TAG, "文件夹：" + modDir.name() +" 的模组：" + mod.getModName() +" 完成加载。");
         }else {
             Log.error(TAG, "文件夹：" + modDir.name() + "中的info.json文件不合法，跳过加载此模组！！！");
         }
-
     }
 
     /**
      * 运行所有mod
     * */
     public void runAllMods () {
-        if (mods.isEmpty()) {
+        if (ModContainer.getInstance().isEmpty()) {
             Log.print(TAG, "没有加载任何模组，跳过运行");
             return;
         }
+
         Log.print(TAG, "开始运行所有的模组……");
-        for (Mod mod : this.mods.values()) {
+        HashMap<String, Mod> allMods = ModContainer.getInstance().getAllMods();
+        for (Mod mod : allMods.values()) {
             mod.run();
         }
     }
@@ -112,7 +118,7 @@ public class ModLoader {
      * */
     private boolean hasMod (JsonValue modInfo) {
         String namespace = modInfo.get("namespace").asString();
-        for (Mod mod : mods.values()) {
+        for (Mod mod : this.getMods().values()) {
             if (Objects.equals(namespace, mod.getModNamespace())){
                 Log.error(TAG,
                     "待加载的模组："+ modInfo.getString("name") +" 的命名空间：" + namespace
@@ -220,7 +226,7 @@ public class ModLoader {
     }
 
     public HashMap<String, Mod> getMods () {
-        return this.mods;
+        return ModContainer.getInstance().getAllMods();
     }
 
     /**
@@ -234,8 +240,22 @@ public class ModLoader {
         return this.root;
     }
 
+    /**
+     *  获取mods应该存在的目录
+     **/
     public FileHandle getRootFile () {
         String storagePath = Gdx.files.getLocalStoragePath();
         return Gdx.files.absolute(storagePath + this.getRoot());
+    }
+
+    private void checkRoot () {
+        Log.print(TAG, "mods文件夹根目录：" + this.getRootFile().toString());
+        if (!this.getRootFile().exists()) {
+            //新建mods文件夹
+            this.getRootFile().mkdirs();
+            Log.print(TAG, "创建mods文件夹");
+        }else {
+            Log.print(TAG, "mods文件夹已经存在：" + this.getRootFile().path() + "，跳过创建");
+        }
     }
 }
