300491f40c8287ad7549761d778b7e6f49239e96
Muxiuesd <m114514012138@outlook.com>
2025-05-11 20:25:33 +0800

    更新：方块实体被破坏后默认掉落所存有的物品

--- core/src/main/java/ttk/muxiuesd/world/block/abs/BlockEntity.java
+++ core/src/main/java/ttk/muxiuesd/world/block/abs/BlockEntity.java
@@ -1,14 +1,22 @@
 package ttk.muxiuesd.world.block.abs;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import ttk.muxiuesd.Fight;
 import ttk.muxiuesd.interfaces.BlockDrawable;
 import ttk.muxiuesd.interfaces.Inventory;
 import ttk.muxiuesd.interfaces.Tickable;
 import ttk.muxiuesd.interfaces.Updateable;
+import ttk.muxiuesd.registrant.Gets;
+import ttk.muxiuesd.system.EntitySystem;
+import ttk.muxiuesd.util.TaskTimer;
+import ttk.muxiuesd.util.Util;
 import ttk.muxiuesd.world.World;
 import ttk.muxiuesd.world.block.BlockPos;
 import ttk.muxiuesd.world.block.InteractResult;
 import ttk.muxiuesd.world.entity.Backpack;
+import ttk.muxiuesd.world.entity.ItemEntity;
 import ttk.muxiuesd.world.entity.Player;
 import ttk.muxiuesd.world.entity.abs.LivingEntity;
 import ttk.muxiuesd.world.item.ItemStack;
@@ -53,7 +61,26 @@ public abstract class BlockEntity implements Updateable, Tickable, BlockDrawable
      * 方块实体被破坏
      * */
     public void beDestroyed (World world, Player destroyer) {
+        EntitySystem es = (EntitySystem) world.getSystemManager().getSystem("EntitySystem");
+        //掉落物品
+        for (int i = 0; i < this.getInventory().getSize(); i++) {
+            ItemStack itemStack = this.getInventory().getItemStack(i);
+            if (itemStack == null) continue;
 
+            itemStack.getItem().beDropped(world, destroyer);
+            ItemEntity itemEntity = (ItemEntity) Gets.ENTITY(Fight.getId("item_entity"), es);
+            itemEntity.setItemStack(itemStack);
+            itemEntity.setPosition(getBlockPos());
+            itemEntity.setSize(ItemEntity.DEFAULT_SIZE);
+            itemEntity.setOnGround(false);
+            itemEntity.setOnAirTimer(new TaskTimer(0.2f, 0, () -> itemEntity.setOnAirTimer(null)));
+            float speed = MathUtils.random(2f, 3.5f);
+            double radian = Util.randomRadian();
+            itemEntity.setSpeed(speed);
+            itemEntity.setVelocity(new Vector2((float) Math.cos(radian), (float) Math.sin(radian)));
+
+            this.getInventory().clear(i);
+        }
     }
 
     /**
